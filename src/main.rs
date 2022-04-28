ce66ffb7e6207bac53b561fddbcbb49a37e790d7
Sebastian Kunert <skunert49@gmail.com>
2022-04-28 17:10:50 +0200

    Initial patch support

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 use camino::Utf8PathBuf;
+use cargo_metadata::Metadata;
+use std::collections::HashMap;
+use std::ffi::OsString;
+use std::io::Write;
 use std::path::PathBuf;
-use std::process::{exit, Command, Stdio};
+use std::process::{self, exit, Command, Stdio};
+use std::str::FromStr;
 use structopt::StructOpt;
+use tempfile::NamedTempFile;
+use toml::macros::IntoDeserializer;
 use toml::Value;
+use toml_edit::{Document, Formatted};
 
 use log::{debug, error, warn};
 
@@ -76,6 +84,9 @@ enum Opts {
             name = "remote options"
         )]
         options: Vec<String>,
+
+        #[structopt(help = "ignore patches", long = "ignore-patches")]
+        ignore_patches: bool,
     },
 }
 
@@ -106,6 +117,223 @@ fn config_from_file(config_path: &Utf8PathBuf) -> Option<Value> {
     Some(value)
 }
 
+pub fn locate_workspace_folder(mut crate_path: PathBuf) -> Result<PathBuf, String> {
+    let cargo = std::env::var("CARGO").unwrap_or("cargo".to_owned());
+    log::debug!("Checking workspace root of path {:?}", crate_path);
+    crate_path.push("Cargo.toml");
+    let output = Command::new(cargo)
+        .arg("locate-project")
+        .arg("--workspace")
+        .arg("--manifest-path")
+        .arg(crate_path.as_os_str().clone())
+        .output()
+        .expect("jojo");
+
+    if !output.status.success() {
+        return Err(format!("{:?}", output.status));
+    }
+
+    let output = String::from_utf8(output.stdout).map_err(|e| e.to_string())?;
+    let parsed = json::parse(&output).map_err(|e| e.to_string())?;
+    let root = parsed["root"].as_str().ok_or(String::from("no root"))?;
+    let mut result = PathBuf::from(root);
+
+    // Remove the trailing "/Cargo.toml"
+    result.pop();
+    Ok(result)
+}
+
+#[derive(Debug, Clone)]
+struct PatchProject {
+    pub name: OsString,
+    pub local_path: PathBuf,
+    pub remote_path: PathBuf,
+}
+
+impl PatchProject {
+    pub fn new(name: OsString, path: PathBuf, remote_path: PathBuf) -> Self {
+        PatchProject {
+            name,
+            local_path: path,
+            remote_path,
+        }
+    }
+}
+
+fn build_patch_projects(
+    mut manifest: &mut Document,
+    patches_folder: PathBuf,
+) -> Option<Vec<PatchProject>> {
+    let mut known_projects: Vec<PatchProject> = Vec::new();
+
+    let patches = manifest["patch"].as_table_mut();
+    if patches.is_none() {
+        log::info!("No patches in project.");
+        return None;
+    }
+    let patches = patches.unwrap();
+    // Project like polkadot or substrate
+    for top_level_item in patches.iter_mut() {
+        let mut crate_level_item = top_level_item.1.as_table_mut();
+        if crate_level_item.is_none() {
+            continue;
+        }
+        let mut crate_level_item = crate_level_item.unwrap();
+        for kv in crate_level_item.iter_mut() {
+            let maybe_table =
+                kv.1.as_value_mut()
+                    .map(|f| f.as_inline_table_mut())
+                    .flatten();
+            if let Some(table) = maybe_table {
+                if let Some(path) = table.get("path") {
+                    let path = PathBuf::from(path.as_str().unwrap().clone());
+                    let mut known_project = known_projects
+                        .iter()
+                        .find(|known_target| path.starts_with(&known_target.local_path))
+                        .cloned();
+                    match known_project {
+                        None => {
+                            // Project needs to be copied
+                            let path_to_copy = locate_workspace_folder(path.clone())
+                                .expect("Can not determine workspace path");
+                            let name = path_to_copy.file_name().unwrap().to_owned();
+                            let mut remote_folder = PathBuf::from("../patches"); //patches_folder.clone();
+                            remote_folder.push(name.clone());
+                            log::info!(
+                                "Found project '{:?}', will copy to '{:?}'",
+                                &path_to_copy,
+                                remote_folder
+                            );
+
+                            known_projects.push(PatchProject::new(
+                                name,
+                                path_to_copy.clone(),
+                                remote_folder.clone(),
+                            ));
+                            let mut new_path = remote_folder.clone();
+                            new_path.push(path.strip_prefix(path_to_copy).expect("Jawoll"));
+                            log::info!("Point {:?} to {:?}", kv.0.to_string(), new_path);
+                            table
+                                .insert("path", toml_edit::Value::from(new_path.to_str().unwrap()));
+                        }
+
+                        Some(patch_target) => {
+                            let mut new_path = patch_target.remote_path.clone();
+                            new_path
+                                .push(path.strip_prefix(patch_target.local_path).expect("Jawoll"));
+                            log::info!("Point {:?} to {:?}", kv.0.to_string(), new_path);
+                            table
+                                .insert("path", toml_edit::Value::from(new_path.to_str().unwrap()));
+                        }
+                    }
+                } else {
+                    log::debug!(
+                        "Ignoring patched crate '{}', not path given.",
+                        kv.0.to_string()
+                    );
+                }
+            }
+        }
+    }
+    log::info!("patches: {:?}", known_projects);
+    Some(known_projects)
+}
+
+fn handle_patches(
+    manifest_path: Utf8PathBuf,
+    mut build_folder: PathBuf,
+) -> Option<(NamedTempFile, Vec<PatchProject>)> {
+    let config_file_string = std::fs::read_to_string(manifest_path)
+        .ok()
+        .expect("Shold work");
+    let mut doc = config_file_string.parse::<Document>().expect("invalid doc");
+    build_folder.push("patches");
+    let maybe_patches = build_patch_projects(&mut doc, build_folder);
+
+    maybe_patches.map(|patches| {
+        let mut temp = NamedTempFile::new().expect("No tempfile for us");
+        temp.write_all(doc.to_string().as_bytes())
+            .expect("Unable to write file");
+        (temp, patches)
+    })
+}
+
+fn copy_patches_to_remote(
+    build_path: &String,
+    build_server: &String,
+    patched_cargo_file: NamedTempFile,
+    projects_to_copy: Vec<PatchProject>,
+) {
+    log::info!(
+        "Found patches in project. Copying {} ({:?}) projects.",
+        projects_to_copy.len(),
+        projects_to_copy
+            .iter()
+            .map(|p| &p.name)
+            .collect::<Vec<&OsString>>()
+    );
+    for patch_operation in projects_to_copy.iter() {
+        let local_proj_path = format!("{}/", patch_operation.local_path.to_string_lossy());
+        let remote_proj_path = format!(
+            "{}:{}",
+            build_server,
+            patch_operation.remote_path.to_string_lossy()
+        );
+        log::info!(
+            "Copying {:?} from {} to {}.",
+            patch_operation.name,
+            &local_proj_path,
+            &remote_proj_path
+        );
+        // transfer project to build server
+        let mut rsync_to = Command::new("rsync");
+        rsync_to
+            .arg("-a")
+            .arg("-q")
+            .arg("--delete")
+            .arg("--compress")
+            .arg(PROGRESS_FLAG)
+            .arg("--exclude")
+            .arg("target")
+            .arg("--exclude")
+            .arg(".*")
+            .arg("--rsync-path")
+            .arg("mkdir -p remote-builds/patches && rsync")
+            .arg(local_proj_path)
+            .arg(remote_proj_path)
+            .stdout(Stdio::inherit())
+            .stderr(Stdio::inherit())
+            .stdin(Stdio::inherit())
+            .output()
+            .unwrap_or_else(|e| {
+                error!("Failed to transfer project to build server (error: {})", e);
+                exit(-4);
+            });
+    }
+
+    let local_toml_path = patched_cargo_file.path().to_string_lossy();
+    let remote_toml_path = format!("{}:{}/Cargo.toml", build_server, build_path);
+    log::debug!(
+        "Copying Cargo.toml from {} to {}.",
+        &local_toml_path,
+        &remote_toml_path
+    );
+    let mut rsync_toml = Command::new("rsync");
+    rsync_toml
+        .arg("-vz")
+        .arg(PROGRESS_FLAG)
+        .arg(local_toml_path.to_string())
+        .arg(remote_toml_path)
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .stdin(Stdio::inherit())
+        .output()
+        .unwrap_or_else(|e| {
+            error!("Failed to transfer project to build server (error: {})", e);
+            exit(-4);
+        });
+}
+
 fn main() {
     simple_logger::SimpleLogger::from_env().init().unwrap();
 
@@ -120,6 +348,7 @@ fn main() {
         hidden,
         command,
         options,
+        ignore_patches,
     } = Opts::from_args();
 
     let mut metadata_cmd = cargo_metadata::MetadataCommand::new();
@@ -136,10 +365,13 @@ fn main() {
             exit(1)
         }
     };
-    let project_dir = project_metadata.workspace_root;
+    let project_dir = project_metadata.workspace_root.clone();
     debug!("Project dir: {:?}", project_dir);
+
     let mut manifest_path = project_dir.clone();
     manifest_path.push("Cargo.toml");
+    log::info!("Manifest_path: {:?}", manifest_path);
+
     let project_name = project_metadata
         .packages
         .iter()
@@ -151,6 +383,10 @@ fn main() {
             },
             |p| &p.name,
         );
+
+    let build_path_folder = "~/remote-builds/";
+    let build_path = format!("{}/{}/", build_path_folder, project_name);
+
     debug!("Project name: {:?}", project_name);
     let configs = vec![
         config_from_file(&project_dir.join(".cargo-remote.toml")),
@@ -164,6 +400,8 @@ fn main() {
             }),
     ];
 
+    let maybe_patches = handle_patches(manifest_path, PathBuf::from(build_path_folder));
+
     // TODO: move Opts::Remote fields into own type and implement complete_from_config(&mut self, config: &Value)
     let build_server = remote
         .or_else(|| {
@@ -176,8 +414,6 @@ fn main() {
             error!("No remote build server was defined (use config file or --remote flag)");
             exit(-3);
         });
-
-    let build_path = format!("~/remote-builds/{}/", project_name);
 
     debug!("Transferring sources to build server.");
     // transfer project to build server
@@ -208,6 +444,11 @@ fn main() {
             error!("Failed to transfer project to build server (error: {})", e);
             exit(-4);
         });
+
+    if let Some((patched_cargo_file, project_list)) = maybe_patches {
+        copy_patches_to_remote(&build_path, &build_server, patched_cargo_file, project_list);
+    }
+
     debug!("Build ENV: {:?}", build_env);
     debug!("Environment profile: {:?}", env);
     debug!("Build path: {:?}", build_path);
